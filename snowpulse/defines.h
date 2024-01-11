e15650296d8b15386e4209d43175974bd0a357f5
One GameDev Saga <onegamedevsaga@gmail.com>
2024-01-11 18:16:00 +0800

    Update submodule

--- snowpulse/defines.h
+++ snowpulse/defines.h
@@ -28,6 +28,13 @@
 
 #define SPSAFE_DELETE(ptr) if(ptr != 0) { delete ptr; ptr=0; }
 
+// Use this in public access modifier
+#define SPSINGLETON_FRIEND(cls) \
+static cls* GetInstance() { return cls::GetSingletonInstance(); } \
+private: \
+friend class snowpulse::Singleton<cls>; \
+public:
+
 #define SPPI (3.14159265358979323846f)
 #define SPDEGTORAD(degrees) (degrees * 0.017453292519943f)
 
